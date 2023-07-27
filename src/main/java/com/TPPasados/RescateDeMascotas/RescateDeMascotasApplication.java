279dcb475459eb0de8982c62cc3df803361b8dfd
RafaelOlmos <37516582+RafaelAlexander@users.noreply.github.com>
2023-07-27 17:02:08 -0300

    Inicio

---
+++ src/main/java/com/TPPasados/RescateDeMascotas/RescateDeMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.TPPasados.RescateDeMascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescateDeMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescateDeMascotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TPPasados/RescateDeMascotas/RescateDeMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TPPasados.RescateDeMascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescateDeMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
